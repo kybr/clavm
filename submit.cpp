13930ff5d4829cc024f32a173c279a7cc77a63f5
Karl Yerkes <karl.yerkes@gmail.com>
2023-11-09 20:34:44 -0800

    cleaning up code; remove submit-cli

--- submit.cpp
+++ submit.cpp
@@ -45,93 +45,11 @@ extern "C" void submit(char* reply, const char* source) {
 }
 
 //
+// There was benchmarking code here once. It tested the semaphores
+// only from submit to clavm to tcc and back. It showed something
+// like this:
 //
+// 1000000 round trip. 8s ellapsed. mean: 0.000009s
+// 1000000 individual. mean: 0.000009s stddev: 0.000002
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-const char* test(const char* code) {
-  // open code shared memory
-  // open semaphores
-  // wait(submit)
-  // [write] CODE
-  // post(compile)
-  // wait(compile)
-  // [read] CODE
-  // post(submit)
-
-  auto* submit = new Semaphore("submit");
-  auto* compile = new Semaphore("compile");
-
-  using std::chrono::duration;
-  using std::chrono::high_resolution_clock;
-  using std::chrono::time_point;
-  time_point<high_resolution_clock> then = high_resolution_clock::now();
-
-  for (int i = 0; i < 1000000; i++) {
-    TRACE("Started submit\n");
-    TRACE("Waiting for 'submit'\n");
-    submit->wait();
-    TRACE("Signaling 'compile'\n");
-    compile->post();
-    TRACE("Waiting on 'compile'\n");
-    compile->wait();
-    TRACE("Signaling 'submit'\n");
-    submit->post();
-    TRACE("Submit done\n");
-  }
-
-  duration<double> time = high_resolution_clock::now() - then;
-
-  printf("time: %lf mean: %lf\n", time.count(), time.count() / 1000000.0);
-  fflush(stdout);
-
-  std::vector<double> list;
-  for (int i = 0; i < 1000000; i++) {
-    using std::chrono::duration;
-    using std::chrono::high_resolution_clock;
-    using std::chrono::time_point;
-    time_point<high_resolution_clock> then = high_resolution_clock::now();
-
-    TRACE("Started submit\n");
-    TRACE("Waiting for 'submit'\n");
-    submit->wait();
-    TRACE("Signaling 'compile'\n");
-    compile->post();
-    TRACE("Waiting on 'compile'\n");
-    compile->wait();
-    TRACE("Signaling 'submit'\n");
-    submit->post();
-    TRACE("Submit done\n");
-
-    duration<double> time = high_resolution_clock::now() - then;
-
-    list.push_back(time.count());
-  }
-
-  double sum = 0;
-  for (int i = 0; i < list.size(); i++) {
-    sum += list[i];
-  }
-  double mean = sum / list.size();
-
-  sum = 0;
-  for (int i = 0; i < list.size(); i++) {
-    double v = list[i] - mean;
-    sum += v * v;
-  }
-  double stddev = sqrt(sum / list.size());
-  printf("mean: %lf stddev: %lf\n", mean, stddev);
-
-  return "got here";
-}
-
-int main() { printf("Got %s\n", test("void play() {}")); }
 
